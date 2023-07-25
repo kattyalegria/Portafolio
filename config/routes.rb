ffab09b4274b9f8907df85f721121bd4e34e6f21
Katty <kattystc@gmail.com>
2023-07-25 21:19:51 +0000

    Correcciones

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get '/home', to: 'pages#home'
+  get '/projects', to: 'pages#projects'
+  get '/contact', to: 'pages#contact'
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'pages#home'
+end
